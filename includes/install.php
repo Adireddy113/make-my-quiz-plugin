f7db50a09fea8e437eedd136b1c4cc024dd6323e
Adi Reddy <syagamreddyadireddy423@gmail.com>
2025-08-07 22:37:42 +0530

    Created db table *mmq_results* to store results

--- includes/install.php
+++ includes/install.php
@@ -8,7 +8,7 @@ global $wpdb;
 $table_name=$wpdb->prefix . 'mmq_results';
 $charset_collate=$wpdb->get_charset_collate();
 
-$sql="CREATE TABLE $table name (
+$sql="CREATE TABLE $table_name (
 id INT(3) NOT NULL AUTO_INCREMENT,
 quiz_id INT NOT NULL,
 user_name VARCHAR(50) NOT NULL,
@@ -16,7 +16,7 @@ score INT NOT NULL,
 total INT NOT NULL,
 submitted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
-)$charset_collate;";
+) $charset_collate;";
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 dbDelta($sql);
